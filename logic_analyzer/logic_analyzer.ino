96cced6fe537ef9fdea840e1744d37aabec25ccd
strah19 <m.strahinja@yahoo.com>
2021-08-11 21:16:08 -0500

    fixed analyzer

--- logic_analyzer/logic_analyzer.ino
+++ logic_analyzer/logic_analyzer.ino
@@ -1,21 +1,17 @@
-#define ADDRESS_1 23
-#define ADDRESS_16 53
-
-#define DATA_1 40
-#define DATA_8 52
-
+int const ADDRESS_LINES[] = { 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53 };
+int const DATA_LINES[] = { 38, 40, 42, 44, 46, 48, 50, 52 };
 #define RW 6
-#define CLK 7
+#define CLK 2
 
 void set_address_lines() {
-  for (int i = ADDRESS_1; i <= ADDRESS_16; i++) {
-    pinMode(i, INPUT);
+  for (int i = 0; i < 16; i++) {
+    pinMode(ADDRESS_LINES[i], INPUT);
   }  
 }
 
 void set_data_lines() {
-  for (int i = DATA_1; i <= DATA_8; i++) {
-    pinMode(i, INPUT);
+  for (int i = 0; i <= 8; i++) {
+    pinMode(DATA_LINES[i], INPUT);
   }  
 }
 
@@ -32,25 +28,27 @@ void setup() {
 }
 
 void on_clock() {
+  char output[15];
   short address = 0;
-  for (int i = ADDRESS_1; i <= ADDRESS_16; i++) {
-    address = (address << 1) + digitalRead(i);
+  for (int i = 15; i >= 0; i--) {
+    int bit = digitalRead(ADDRESS_LINES[i]) ? 1 : 0;
+    Serial.print(bit);
+    address = (address << 1) + bit;
   }
+  
+  Serial.print("   ");
 
   byte data = 0;
-  for (int i = DATA_1; i <= DATA_8; i++) {
-    data = (data << 1) + digitalRead(i);
+  for (int i = 7; i >= 0; i--) {
+    int bit = digitalRead(DATA_LINES[i]) ? 1 : 0;
+    Serial.print(bit);
+    data = (data << 1) + bit;
   }
 
-  Serial.print(address, BIN);
-  Serial.print(" ");
-  Serial.print(data, BIN);
-  Serial.print(" ");
-  Serial.print((digitalRead(RW)) ? "W" : "R");
-  Serial.print(" ");
-  Serial.print(address, HEX);
-  Serial.print(" ");
-  Serial.println(data, HEX);
+  Serial.print("   ");
+
+  sprintf(output, "   %04x  %c %02x", address, digitalRead(RW) ? 'r' : 'W', data);
+  Serial.println(output);  
 }
 
-void loop() {  }
+void loop() { }
