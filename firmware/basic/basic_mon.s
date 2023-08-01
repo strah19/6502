7fe804d0fdd6fe1e774828d19dccfceb698c75dd
strah19 <m.strahinja@yahoo.com>
2023-07-31 19:44:03 -0500

    cleaning basic

--- firmware/basic/basic_mon.s
+++ firmware/basic/basic_mon.s
@@ -1,81 +1,64 @@
-; minimal monitor for EhBASIC and 6502 simulator V1.05
-; tabs converted to space, tabwidth=6
-
-; To run EhBASIC on the simulator load and assemble [F7] this file, start the simulator
-; running [F6] then start the code with the RESET [CTRL][SHIFT]R. Just selecting RUN
-; will do nothing, you'll still have to do a reset to run the code.
-
 #include "basic.s"
 
 ; put the IRQ and MNI code in RAM so that it can be changed
-
 IRQ_vec     = VEC_SV+2        ; IRQ code vector
 NMI_vec     = IRQ_vec+$0A     ; NMI code vector
 LF  = $0A        ; Line feed character
 
-; setup for the 6502 simulator environment
-
+; setup serial interface registers
 ACIA_DATA = $7F00
 ACIA_STATUS = $7F01
 ACIA_COMMAND = $7F02
 ACIA_CONTROL = $7F03
 
-; now the code. all this does is set up the vectors and interrupt code
-; and wait for the user to select [C]old or [W]arm start. nothing else
-; fits in less than 128 bytes
+basic_reset:
+	CLD                     ; clear decimal mode
+	LDX   #$FF              ; empty stack
+	TXS                     ; set the stack
 
-; reset vector points here
+	lda #$00
+	sta ACIA_STATUS             ;Soft reset
 
-RES_vec
-      CLD                     ; clear decimal mode
-      LDX   #$FF              ; empty stack
-      TXS                     ; set the stack
+	lda #$0b				;No parity, no echo, no interrupt
+	sta ACIA_COMMAND
 
-    lda #$00
-    sta ACIA_STATUS             ;Soft reset
-
-    lda #$0b				;No parity, no echo, no interrupt
-    sta ACIA_COMMAND
-
-    lda #$1f				;1 stop bit, 8 data bits, 19200 baud
-    sta ACIA_CONTROL
+	lda #$1f				;1 stop bit, 8 data bits, 19200 baud
+	sta ACIA_CONTROL
 
 ; set up vectors and interrupt code, copy them to page 2
 
-      LDY   #END_CODE-LAB_vec ; set index/count
+	LDY   #END_CODE-LAB_vec ; set index/count
 LAB_stlp
-      LDA   LAB_vec-1,Y       ; get byte from interrupt code
-      STA   VEC_IN-1,Y        ; save to RAM
-      DEY                     ; decrement index/count
-      BNE   LAB_stlp          ; loop if more to do
+	LDA   LAB_vec-1,Y       ; get byte from interrupt code
+	STA   VEC_IN-1,Y        ; save to RAM
+	DEY                     ; decrement index/count
+	BNE   LAB_stlp          ; loop if more to do
 
 ; now do the signon message, Y = $00 here
 
 LAB_signon
-      LDA   LAB_mess,Y        ; get byte from sign on message
-      BEQ   LAB_nokey         ; exit loop if done
+	LDA   LAB_mess,Y        ; get byte from sign on message
+	BEQ   LAB_nokey         ; exit loop if done
 
-      JSR   V_OUTP            ; output character
-      INY                     ; increment index
-      BNE   LAB_signon        ; loop, branch always
+	JSR   V_OUTP            ; output character
+	INY                     ; increment index
+	BNE   LAB_signon        ; loop, branch always
 
 LAB_nokey
-      JSR   V_INPT            ; call scan input device
-      BCC   LAB_nokey         ; loop if no key
+	JSR   V_INPT            ; call scan input device
+	BCC   LAB_nokey         ; loop if no key
 
-      AND   #$DF              ; mask xx0x xxxx, ensure upper case
-      CMP   #'W'              ; compare with [W]arm start
-      BEQ   LAB_dowarm        ; branch if [W]arm start
+	AND   #$DF              ; mask xx0x xxxx, ensure upper case
+	CMP   #'W'              ; compare with [W]arm start
+	BEQ   LAB_dowarm        ; branch if [W]arm start
 
-      CMP   #'C'              ; compare with [C]old start
-      BNE   RES_vec           ; loop if not [C]old start
+	CMP   #'C'              ; compare with [C]old start
+	BNE   basic_reset           ; loop if not [C]old start
 
-      JMP   LAB_COLD          ; do EhBASIC cold start
+	JMP   LAB_COLD          ; do EhBASIC cold start
 
 LAB_dowarm
-      JMP   LAB_WARM          ; do EhBASIC warm start
-
-; byte out to simulated ACIA
+	JMP   LAB_WARM          ; do EhBASIC warm start
 
 ACIAout
     CMP     #LF                     ; Ignore line feed character
@@ -89,8 +72,6 @@ TXDELAY:
     PLA                    ; Restore A.
 Ignore:
       RTS
-
-; byte in from simulated ACIA
 
 ACIAin
     lda ACIA_STATUS
@@ -325,6 +306,3 @@ DONE
       .word NMI_vec           ; NMI vector
       .word RESET           ; RESET vector
       .word IRQ_vec           ; IRQ vector
-
-      .end RES_vec            ; set start at reset vector
-      
